93ce99ef3319c9ead5ef39030e61c7a69002a7ce
boxi <2909027418@qq.com>
2022-07-08 23:07:37 +0800

    modify table name

--- pkg/utils/colname.go
+++ pkg/utils/colname.go
@@ -9,22 +9,22 @@ import (
 //  返回collection名字
 // count
 func CountCol(node, bcname string) string {
-	return fmt.Sprintf("count_%s_%s", MD5(node, bcname), bcname)
+	return fmt.Sprintf("count_%s_%s", bcname, node)
 }
 
 // block
 func BlockCol(node, bcname string) string {
-	return fmt.Sprintf("block_%s_%s", MD5(node, bcname), bcname)
+	return fmt.Sprintf("block_%s_%s", bcname, node)
 }
 
 // tx
 func TxCol(node, bcname string) string {
-	return fmt.Sprintf("tx_%s_%s", MD5(node, bcname), bcname)
+	return fmt.Sprintf("tx_%s_%s", bcname, node)
 }
 
 // account
 func AccountCol(node, bcname string) string {
-	return fmt.Sprintf("account_%s_%s", MD5(node, bcname), bcname)
+	return fmt.Sprintf("account_%s_%s", bcname, node)
 }
 
 // 对 node 和 bcname 做个md5
